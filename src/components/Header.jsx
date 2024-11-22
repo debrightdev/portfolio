e440442716bdab7efc84e55b5eda6f6148c314b0
Augustine Michael <debrightdev@gmail.com>
2024-11-22 06:37:16 -0800

    update

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import icon from '../assets/img/icon.png';
+
+const Header = () => {
+  return (
+    <>
+      <header className='flex-between align-center w-90 mx-auto pt-16 pb-16'>
+        <div className='flex align-center'>
+          <img className='w-50px' src={icon} alt='' />
+          <div>
+            <h3 className='text-14'>Augustine Michael</h3>
+            <p className='text-12'>Software Engineer (Web)</p>
+          </div>
+        </div>
+        <ul className='flex align-center gap-40 justify-center font-semibold'>
+          <li><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Projects</a></li>
+        </ul>
+        <button className='font-semibold'>Somewhere training DNN models.</button>
+      </header>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <div className='content w-90 mx-auto pt-100'>
+        <h1 className='text-48 mb-20'>Pushing Machines to Brilliance.</h1>
+        <div>
+          <span className='text-18 block mb-20'>
+            Web dev{' '}
+            <a
+              className='text-primary'
+              target='_blank'
+              href='https://debrightweb.com'
+            >
+              @debrightweb ↗
+            </a>{' '}
+          </span>
+          <p className='text-18 block mb-20 max-w-600'>
+            Software Engineer - Building ML Architectures + Models, Backend +
+            Infrastructure Systems & Web + Mobile Apps (Light Work).
+          </p>
+          <p className='text-18 block mb-20 max-w-600'>The only way to do great work is to love what you do.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
